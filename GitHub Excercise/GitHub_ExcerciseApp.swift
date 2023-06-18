1abab7ec2c68b2dd44d2f58140489d7a1763d165
Bob Witmer <bwitmer@golden.net>
2023-06-18 19:51:44 -0400

    Initial Commit

---
+++ GitHub Excercise/GitHub_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcerciseApp.swift
+//  GitHub Excercise
+//
+//  Created by Bob Witmer on 2023-06-18.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
